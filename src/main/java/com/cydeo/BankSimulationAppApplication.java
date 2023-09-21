c11930cf8f8a895268aedfc948ccdf5546e3710b
amiritahmed <amiritahmed@gmail.com>
2023-09-20 21:35:56 -0400

    init

---
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -0,0 +1,16 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BankSimulationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSimulationAppApplication.class, args);
+    }
+
+}
